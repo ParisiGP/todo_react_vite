bff8abc04a0abff852d512e7816a890be85c4c2c
ParisiGP <gustavoparisi12@gmail.com>
2023-11-28 15:40:01 -0300

    nova tentativa

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 
+import { IoMdArrowRoundBack } from "react-icons/io";
+
 
 import './App.css'
 import Todo from './components/Todo';
@@ -63,20 +65,34 @@ function App() {
     setTodos(newTodos);
   };
 
-  return <div className='app'>
-    <h1>Lista de Tarefas</h1>
-    <Search search={search} setSearch={setSearch} />
-    <Filter filter={filter} setFilter={setFilter} setSort={setSort}/>
-    <div className="todo-list">
-      {todos.filter((todo) => filter === "All" ? true : filter ==="Completed"? todo.isCompleted : !todo.isCompleted)
-      .filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase()))
-      .sort((a, b) => sort === "Asc" ? a.text.localeCompare(b.text) : b.text.localeCompare(a.text))
-      .map((todo) => (
-        <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
-      ))}
+  return (
+
+    <div className="body">
+
+
+      <a className='back' href="https://parisigp.github.io/MyPortfolio/">
+        <IoMdArrowRoundBack />
+      </a>
+      <div className='app'>
+        <h1>Lista de Tarefas</h1>
+        <Search search={search} setSearch={setSearch} />
+        <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
+        <div className="todo-list">
+          {todos.filter((todo) => filter === "All" ? true : filter === "Completed" ? todo.isCompleted : !todo.isCompleted)
+            .filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase()))
+            .sort((a, b) => sort === "Asc" ? a.text.localeCompare(b.text) : b.text.localeCompare(a.text))
+            .map((todo) => (
+              <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo} />
+            ))}
+        </div>
+        <TodoForm addTodo={addTodo} />
+      </div>
     </div>
-    <TodoForm addTodo={addTodo} />
-  </div>;
+
+
+
+
+  );
 
 }
 
